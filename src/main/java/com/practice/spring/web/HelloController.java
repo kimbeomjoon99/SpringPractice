45f368c6ef8019893a36bd7631ed45b701197ef4
kimbeomjoon99 <tnldls99@naver.com>
2024-03-01 16:31:29 +0900

    Feat: implement DTO response feature

--- src/main/java/com/practice/spring/web/HelloController.java
+++ src/main/java/com/practice/spring/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.practice.spring.web;
 
+import com.practice.spring.web.dto.HelloResponceDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponceDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount)
+    {
+        return new HelloResponceDto(name, amount);
     }
 }
