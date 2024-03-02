ded91aa94a359d78c92b676e3d23f63c31ebea1c
kimbeomjoon99 <tnldls99@naver.com>
2024-03-02 22:14:54 +0900

    Feat: implement post update feature

--- src/main/java/com/practice/spring/service/posts/PostsService.java
+++ src/main/java/com/practice/spring/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.practice.spring.service.posts;
 
 import com.practice.spring.domain.posts.PostsRepository;
+import com.practice.spring.domain.posts.Posts;
+import com.practice.spring.web.dto.PostsResponseDto;
 import com.practice.spring.web.dto.PostsSaveRequestDto;
+import com.practice.spring.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +18,23 @@ public class PostsService {
     @Transactional
     public long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
     }
 
 }
---
+++ src/main/java/com/practice/spring/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,21 @@
+package com.practice.spring.web.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+}
