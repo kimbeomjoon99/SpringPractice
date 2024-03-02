c5d1bd7ac54ec31e42332b8c678db812907ee2b9
kimbeomjoon99 <tnldls99@naver.com>
2024-03-02 21:55:27 +0900

    Feat: implement API for posting service

---
+++ src/main/java/com/practice/spring/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.practice.spring.service.posts;
+
+import com.practice.spring.domain.posts.PostsRepository;
+import com.practice.spring.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
---
+++ src/main/java/com/practice/spring/web/PostsApiController.java
@@ -0,0 +1,21 @@
+package com.practice.spring.web;
+
+import com.practice.spring.service.posts.PostsService;
+import com.practice.spring.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+}
