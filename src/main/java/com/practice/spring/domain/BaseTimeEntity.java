692e01646c2e93fabf8f952dabe7bfda1761fcec
kimbeomjoon99 <tnldls99@naver.com>
2024-03-03 16:52:56 +0900

    Feat: implement datetime log feature

---
+++ src/main/java/com/practice/spring/domain/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package com.practice.spring.domain;
+
+import jakarta.persistence.Column;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
--- src/main/java/com/practice/spring/domain/posts/Posts.java
+++ src/main/java/com/practice/spring/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.practice.spring.domain.posts;
 
+import com.practice.spring.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import jakarta.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
