01f53adcec004b736e21031b8da08b87d7f2abba
kimbeomjoon99 <tnldls99@naver.com>
2024-03-04 22:00:23 +0900

    Feat: implement all post referencing feature

--- src/main/java/com/practice/spring/domain/posts/PostsRepository.java
+++ src/main/java/com/practice/spring/domain/posts/PostsRepository.java
@@ -1,7 +1,12 @@
 package com.practice.spring.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long>{
+
+    @Query(value = "SELECT * FROM POSTS p ORDER BY p.id DESC", nativeQuery = true)
+    List<Posts> findAllDesc();
 
 }
--- src/main/java/com/practice/spring/service/posts/PostsService.java
+++ src/main/java/com/practice/spring/service/posts/PostsService.java
@@ -1,5 +1,6 @@
 package com.practice.spring.service.posts;
 
+import com.practice.spring.web.dto.PostsListResponseDto;
 import com.practice.spring.domain.posts.PostsRepository;
 import com.practice.spring.domain.posts.Posts;
 import com.practice.spring.web.dto.PostsResponseDto;
@@ -8,6 +9,8 @@ import com.practice.spring.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,6 +38,13 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }
